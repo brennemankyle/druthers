6e851e3da4eabd50c0106ddfa06dd348ff63fa03
Kyle Brenneman <brenneman.kyle@gmail.com>
2019-07-15 17:52:32 -0600

    Remove welcome storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
-import { Welcome } from '@storybook/react/demo'
 
-import './welcome.stories'
 import '../components/Select/Select.stories'
 import '../components/SelectionWrapper/SelectionWrapper.stories'
