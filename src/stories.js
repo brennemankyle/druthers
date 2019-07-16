abe8473749a639cf98fc869b110b2e35d53349f0
Kyle Brenneman <brenneman.kyle@gmail.com>
2019-07-15 18:18:36 -0600

    Cleanup ejected items

---
+++ src/stories.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+import './components/Select/Select.stories'
+import './components/SelectionWrapper/SelectionWrapper.stories'
--- src/stories/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-
-import '../components/Select/Select.stories'
-import '../components/SelectionWrapper/SelectionWrapper.stories'
