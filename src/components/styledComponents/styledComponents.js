c2b5b7cc07f5287890a25419209c869f37eb57f5
Kyle Brenneman <brenneman.kyle@gmail.com>
2019-08-23 21:25:32 -0600

    use emotion instead of styled-components

--- src/components/styledComponents/styledComponents.js
+++ src/components/styledComponents/styledComponents.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from '@emotion/styled'
 import Item from '../Item/Item'
 import ItemList from '../ItemList/ItemList'
 import RawSearch from '../Search/Search'
