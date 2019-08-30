940d4e0552a1cef17329a35085d02aa946063f16
Kyle Brenneman <brenneman.kyle@gmail.com>
2019-08-30 15:14:10 -0600

    Remove select from storybook for now

--- src/stories.js
+++ src/stories.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 import './components/NewInput/NewInput.stories'
-import './components/Select/Select.stories'
+// import './components/Select/Select.stories'
 import './components/SelectionWrapper/SelectionWrapper.stories'
