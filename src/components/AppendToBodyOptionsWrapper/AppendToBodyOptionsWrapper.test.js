aba196c9b3c9927f76f35a8edb01ec59b5b3d01a
Kyle Brenneman <brenneman.kyle@gmail.com>
2020-03-08 13:56:07 -0600

    improve currect tests

--- src/components/AppendToBodyOptionsWrapper/AppendToBodyOptionsWrapper.test.js
+++ src/components/AppendToBodyOptionsWrapper/AppendToBodyOptionsWrapper.test.js
@@ -6,29 +6,6 @@ import AppendToBodyOptionsWrapper from './AppendToBodyOptionsWrapper'
 import useRefRect from '../../hooks/useRefRect/useRefRect'
 import usePlaceAbove from '../../hooks/usePlaceAbove/usePlaceAbove'
 
-jest.mock('../../hooks/useRefRect/useRefRect', () => {
-  return () => {
-    return {
-      "bottom": 80,
-      "height": 80,
-      "left": 0,
-      "right": 200,
-      "top": 0,
-      "width": 200,
-      "x": 0,
-      "y": 600,
-    }
-  }
-})
-
-jest.mock('../../hooks/usePlaceAbove/usePlaceAbove', () => {
-  return () => {
-    return true
-  }
-})
-
-global.console.error = jest.fn()
-
 it('renders', () => {
   const parentRef = React.createRef()
   const wrapper = mount(<AppendToBodyOptionsWrapper
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,18 @@ import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() })
+
+global.console.error = jest.fn()
+
+jest.mock('./hooks/useRefRect/useRefRect', () => () => ({
+  "bottom": 80,
+  "height": 80,
+  "left": 0,
+  "right": 200,
+  "top": 0,
+  "width": 200,
+  "x": 0,
+  "y": 600,
+}))
+
+jest.mock('./hooks/usePlaceAbove/usePlaceAbove', () => () => true)
