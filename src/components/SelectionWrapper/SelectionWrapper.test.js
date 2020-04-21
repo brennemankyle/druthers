a4927633058d52c866c4c65d93760c635aa904fe
Kyle Brenneman <brenneman.kyle@gmail.com>
2020-04-21 17:35:21 -0600

    Add all tests files

--- src/components/SelectionWrapper/SelectionWrapper.test.js
+++ src/components/SelectionWrapper/SelectionWrapper.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import toJson from "enzyme-to-json";
 import { mockStyles, MockElement, MockInput, MockSvg } from "../../mocks";
 import SelectionWrapper from "./SelectionWrapper";
@@ -47,23 +47,23 @@ it("renders disabled", () => {
 });
 
 // TODO fix: ref searchRef.current.blur() doesn't continue event propogation
-// it("should close", done => {
-//   const wrapper = mount(
-//     <SelectionWrapper
-//       {...mockStyles}
-//       className="test"
-//       areOptionsOpen={true}
-//       onFocus={onFocus}
-//       onBlur={onBlur}
-//       SelectionList={<MockElement />}
-//       Search={<MockInput />}
-//       svg_Expand={MockSvg}
-//     />
-//   );
-//
-//   setTimeout(() => {
-//     wrapper.find("input").simulate("click");
-//     expect(onBlur).toBeCalled();
-//     done();
-//   });
-// });
+it("should close", done => {
+  const wrapper = mount(
+    <SelectionWrapper
+      {...mockStyles}
+      className="test"
+      areOptionsOpen={true}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      SelectionList={<MockElement />}
+      Search={<MockInput />}
+      svg_Expand={MockSvg}
+    />
+  );
+
+  setTimeout(() => {
+    wrapper.find("input").simulate("click");
+    expect(onBlur).toBeCalled();
+    done();
+  });
+});
